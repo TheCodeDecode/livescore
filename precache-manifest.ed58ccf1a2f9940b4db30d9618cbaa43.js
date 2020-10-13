4f8c64a886614d7b06abc0cecc0fe27daf096f1b
TheCodeDecode <akshaykumarr00894@gmail.com>
2020-10-13 16:26:35 +0530

    Updates

---
+++ precache-manifest.ed58ccf1a2f9940b4db30d9618cbaa43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ea31caac4c2e25a1bec1cfc87454f4",
+    "url": "/livescore/index.html"
+  },
+  {
+    "revision": "a7cc401c1ed547ffda8b",
+    "url": "/livescore/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "be2ecc29d0de5c4cca2c",
+    "url": "/livescore/static/css/main.c8b77ef1.chunk.css"
+  },
+  {
+    "revision": "a7cc401c1ed547ffda8b",
+    "url": "/livescore/static/js/2.f47ca16b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/livescore/static/js/2.f47ca16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2ecc29d0de5c4cca2c",
+    "url": "/livescore/static/js/main.80b816fd.chunk.js"
+  },
+  {
+    "revision": "89cfd78c17f850e31cc4",
+    "url": "/livescore/static/js/runtime-main.1368b367.js"
+  },
+  {
+    "revision": "59a9d2e5c0ff7c6bf261179bb4b30042",
+    "url": "/livescore/static/media/vs.59a9d2e5.png"
+  }
+]);
